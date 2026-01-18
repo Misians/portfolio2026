c454a8c3cd6401f2bee8c55e5d92dc24603d779e
misians <artemisiakimberlly@alu.uern.br>
2026-01-18 13:39:39 -0300

    deploy gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Substitua 'NOME-DO-SEU-REPO' pelo nome exato que você criou no GitHub
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio/", 
 })
