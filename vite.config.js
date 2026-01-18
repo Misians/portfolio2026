9147d0a84cdb9c3ba712dfa54cf1b101312bf884
misians <artemisiakimberlly@alu.uern.br>
2026-01-18 13:42:35 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Substitua 'NOME-DO-SEU-REPO' pelo nome exato que você criou no GitHub
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio/", 
+  base: "/portfolio2026/", 
 })
